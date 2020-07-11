314abc76991c82c9f0d3f320cf08d078fd828aa0
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-07-11 09:40:39 +0000

    added navbar to base.html & rendered base.html in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Foodology'
+    return render_template("base.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
