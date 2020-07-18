7ab3f923aebc6aa693769e56b0376a106b080bd6
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-07-18 17:28:56 +0000

    added register.html and added various styling

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def check():
+def index():
     return render_template("index.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
