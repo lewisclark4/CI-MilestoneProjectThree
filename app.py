2f56af257c71f5c9bedd5ac8f4bb22dde7f3ffcf
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-07-19 09:46:48 +0000

    Connect to MongoDB, created environment variables in env.py and added to .gitignore

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import os
+from os import path
 from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
