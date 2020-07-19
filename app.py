4ab59121af6f2e50ac7dc855453da0d82baa088c
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-07-19 10:53:40 +0000

    began to build on register function to store username and password in MongoDB

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import os
 from os import path
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session, redirect, flash
 from flask_pymongo import PyMongo
+import bcrypt
 
 if path.exists("env.py"):
   import env 
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["MONGO_URI"] = "mongodb+srv://lcadmin:Password1@myfirstcluster-y3iip.mongodb.net/foodology?retryWrites=true&w=majority"
+app.config["MONGO_DBNAME"] = "foodology"
+app.config["SECRET_KEY"] = "MYSUPERSECRETKEY123"
 
 mongo = PyMongo(app)
 
@@ -19,9 +20,29 @@ mongo = PyMongo(app)
 def index():
     return render_template("index.html")
 
-@app.route('/register')
+
+## https://www.youtube.com/watch?v=vVx1737auSE
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        users = mongo.db.users
+        user_exists = users.find_one(
+                      {'username': request.form['username']})
+
+        if user_exists is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'),
+                                     bcrypt.gensalt())
+            users.insert({'username': request.form['username'].lower(),
+                         'password': hashpass})
+            session['username'] = request.form['username'].lower()
+            return redirect(url_for('index'))
+
+        session.pop('username', None)
+        return render_template('register.html')
+
+    return render_template('register.html')
+
 
 @app.route('/login')
 def login():
