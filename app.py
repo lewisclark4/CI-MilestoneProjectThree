b0e2631cc9b0f557307b01f1719761a3e6771348
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-07-25 10:08:41 +0000

    fixed insert recipe function so that the public flag is stored correctly

--- app.py
+++ app.py
@@ -88,7 +88,8 @@ def insert_recipe():
     data.update({'ingredients': request.form.getlist('ingredients')})
     data.update({'preparation': request.form.getlist('preparation')})
     data.update({'allergens': request.form.getlist('allergens')})
-    if request.form.getlist('public')[0] == 'checked':
+    # https://stackoverflow.com/questions/31859903/get-the-value-of-a-checkbox-in-flask
+    if request.form.getlist('public'):
         data.update({'public': True}) 
     else:
         data.update({'public': False}) 
